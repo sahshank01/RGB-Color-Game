2482ee56fc19e33d75b49c323b0c181235ed94a5
shashank <shashanksah143@gmail.com>
2020-08-12 14:31:47 +0530

    UI change and added click event handler to color boxes

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
 //variable declerations
-var randomColors=[];
-var colorBoxes=document.getElementsByClassName("square");
-var answer=null;
-var headerRgbDisplay= document.getElementById("colorId");
+var randomColors = [];
+var colorBoxes = document.getElementsByClassName("square");
+var answer = null;
+var headerRgbDisplay = document.getElementById("colorId");
 
 
 //function declerations
-function colorValueGenerator(){
-  return Math.ceil(Math.random()*255);
+function colorValueGenerator() {
+  return Math.ceil(Math.random() * 255);
 }
 
-function randomColorGenerator(){
-  return "rgb( "+colorValueGenerator()+", "+colorValueGenerator()+", "+colorValueGenerator()+")";
+function randomColorGenerator() {
+  return "rgb( " + colorValueGenerator() + ", " + colorValueGenerator() + ", " + colorValueGenerator() + ")";
 }
 
-function fillRandomColorArray(){
-  for(let i=0;i<colorBoxes.length;i++){
+function fillRandomColorArray() {
+  for (let i = 0; i < colorBoxes.length; i++) {
     randomColors.push(randomColorGenerator());
   }
 }
 
-function assignColorToSquares(){
-  for(let i=0;i<colorBoxes.length;i++){
-    colorBoxes[i].style.backgroundColor=randomColors[i];
+function onSuccess() {
+  for (let i = 0; i < colorBoxes.length; i++){
+    colorBoxes[i].style.backgroundColor=answer;
+    colorBoxes[i].style.visibility="visible";
+  }
+}
+function onFaliure(selectedSquare) {
+  selectedSquare.style.visibility="hidden";
+}
+
+function validateUserClick() {
+  if (this.style.backgroundColor === answer)
+    onSuccess();
+  else
+    onFaliure(this);
+}
+function assignColorAndEventListenerToSquares() {
+  for (let i = 0; i < colorBoxes.length; i++) {
+    colorBoxes[i].style.backgroundColor = randomColors[i];
+    colorBoxes[i].addEventListener("click", validateUserClick);
   }
 }
 
+
 //function calling and manipulation
 fillRandomColorArray();
-assignColorToSquares();
+assignColorAndEventListenerToSquares();
 
-answer=colorBoxes[Math.ceil(Math.random()*5)].style.backgroundColor;
-headerRgbDisplay.innerHTML=answer;
+answer = colorBoxes[Math.ceil(Math.random() * 5)].style.backgroundColor;
+headerRgbDisplay.innerHTML = answer;
