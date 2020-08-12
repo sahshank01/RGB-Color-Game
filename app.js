8fafe9217737f7b7b74dde4fa2cf3c08130491c3
shashank <shashanksah143@gmail.com>
2020-08-12 15:29:45 +0530

    added bootstrap-4, fontawsome-4 and navbar with buttons

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var randomColors = [];
 var colorBoxes = document.getElementsByClassName("square");
 var answer = null;
 var headerRgbDisplay = document.getElementById("colorId");
-
+document.getElementById("newGame").addEventListener("click",startGame);
 
 //function declerations
 function colorValueGenerator() {
@@ -15,19 +15,22 @@ function randomColorGenerator() {
 }
 
 function fillRandomColorArray() {
+  randomColors=[];
   for (let i = 0; i < colorBoxes.length; i++) {
     randomColors.push(randomColorGenerator());
   }
 }
 
 function onSuccess() {
-  for (let i = 0; i < colorBoxes.length; i++){
-    colorBoxes[i].style.backgroundColor=answer;
-    colorBoxes[i].style.visibility="visible";
+  document.getElementById("message").innerHTML='Congratulation!!! <i class="fa fa-smile-o fa-lg" aria-hidden="true"></i>';
+  for (let i = 0; i < colorBoxes.length; i++) {
+    colorBoxes[i].style.backgroundColor = answer;
+    colorBoxes[i].style.visibility = "visible";
   }
 }
 function onFaliure(selectedSquare) {
-  selectedSquare.style.visibility="hidden";
+  document.getElementById("message").innerHTML='Try Again <i class="fa fa-frown-o fa-lg" aria-hidden="true"></i>';
+  selectedSquare.style.visibility = "hidden";
 }
 
 function validateUserClick() {
@@ -39,14 +42,18 @@ function validateUserClick() {
 function assignColorAndEventListenerToSquares() {
   for (let i = 0; i < colorBoxes.length; i++) {
     colorBoxes[i].style.backgroundColor = randomColors[i];
+    colorBoxes[i].style.visibility = "visible";
     colorBoxes[i].addEventListener("click", validateUserClick);
   }
 }
 
+function startGame() {
+  document.getElementById("message").innerHTML='All The Best <i class="fa fa-thumbs-up" aria-hidden="true"></i>';
+  fillRandomColorArray();
+  assignColorAndEventListenerToSquares();
+  answer = colorBoxes[Math.ceil(Math.random() * randomColors.length)].style.backgroundColor;
+  headerRgbDisplay.innerHTML = answer;
+}
 
-//function calling and manipulation
-fillRandomColorArray();
-assignColorAndEventListenerToSquares();
-
-answer = colorBoxes[Math.ceil(Math.random() * 5)].style.backgroundColor;
-headerRgbDisplay.innerHTML = answer;
+//function calling
+startGame(this);
